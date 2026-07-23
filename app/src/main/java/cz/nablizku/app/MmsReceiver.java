b47c4995bf1b7991f720320d377d24e8cabce81d
czechoslovakiaserver-pixel <czechoslovakiaserver@gmail.com>
2026-07-23 18:24:13 +0200

    Create MmsReceiver.java

---
+++ app/src/main/java/cz/nablizku/app/MmsReceiver.java
@@ -0,0 +1,11 @@
+package cz.nablizku.app;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MmsReceiver extends BroadcastReceiver {
+    @Override public void onReceive(Context context, Intent intent) {
+        // MMS dekódování bude doplněno po testu konkrétního operátora a telefonu.
+    }
+}

e39ca5ee156dc4ef255b1e64e46b8f52295620ff
czechoslovakiaserver-pixel <czechoslovakiaserver@gmail.com>
2026-07-23 18:24:19 +0200

    Create RespondViaMessageService.java

---
+++ app/src/main/java/cz/nablizku/app/RespondViaMessageService.java
@@ -0,0 +1,9 @@
+package cz.nablizku.app;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class RespondViaMessageService extends Service {
+    @Override public IBinder onBind(Intent intent) { return null; }
+}
